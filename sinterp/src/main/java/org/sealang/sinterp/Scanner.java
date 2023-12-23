e81ff2ec30b4302be54fab640f5a6da3e05888b2
seaeast2 <seaeast2@gmail.com>
2023-12-23 19:30:54 +0900

    Scanner에 식별자 및 키워드 처리기능 추가

--- sinterp/src/main/java/org/sealang/sinterp/Scanner.java
+++ sinterp/src/main/java/org/sealang/sinterp/Scanner.java
@@ -1,7 +1,9 @@
 package org.sealang.sinterp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.sealang.sinterp.TokenType.*;
 
@@ -9,9 +11,31 @@ public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
-    private int start = 0; // lexeme의 첫번째 문자
+    private int start = 0; // lexeme 의 첫번째 문자
     private int current = 0; // 현재의 위치
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -119,11 +143,26 @@ public class Scanner {
                 if (isDigit(c)) {
                     number();
                 }
+                else if(isAlpah(c)) {
+                    identifier();
+                }
                 else {
                     SInterp.error(line, "Unexpected character.");
                 }
                 break;
         }
+    }
+
+    private void identifier() {
+        while(isAlphaNumeric(peek()))
+            advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null)
+            type = IDENTIFIER;
+
+        addToken(type);
     }
 
     private void number() {
@@ -182,6 +221,16 @@ public class Scanner {
         if (current + 1 >= source.length())
             return '\0';
         return source.charAt(current + 1);
+    }
+
+    private boolean isAlpah(char c) {
+        return  (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                (c == '_');
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpah(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
