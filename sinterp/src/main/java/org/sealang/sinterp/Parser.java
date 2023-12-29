c6d74663d7eea968d269d7b747281b9a54213ef2
seaeast2 <seaeast2@gmail.com>
2023-12-29 16:27:16 +0900

    Parser 에 기본 문법 구현

---
+++ sinterp/src/main/java/org/sealang/sinterp/Parser.java
@@ -0,0 +1,149 @@
+package org.sealang.sinterp;
+
+import java.util.List;
+
+import static org.sealang.sinterp.TokenType.*;
+
+/*
+전체 문법 :
+    expression → equality ;
+    equality → comparison ( ( "!=" | "==" ) comparison )* ;
+    comparison → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+    term → factor ( ( "-" | "+" ) factor )* ;
+    factor → unary ( ( "/" | "*" ) unary )* ;
+    unary → ( "!" | "-" ) unary | primary ;
+    primary → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
+ */
+
+// 재귀 하강 파서
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    // expression → equality ;
+    private Expr expression() {
+        return equality();
+    }
+
+    // equality → comparison ( ( "!=" | "==" ) comparison )* ;
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    // comparison → term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    //term → factor ( ( "-" | "+" ) factor )* ;
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    // factor → unary ( ( "/" | "*" ) unary )* ;
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(SLASH, STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    // unary → ( "!" | "-" ) unary | primary ;
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    // primary → NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
+    private Expr primary() {
+        if (match(FALSE))
+            return new Expr.Literal(false);
+        if (match(TRUE))
+            return new Expr.Literal(true);
+        if (match(NIL))
+            return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(LPAREN)) {
+            Expr expr = expression();
+            consume(RPAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean check(TokenType type) { // token 을 소비하지 않는다.
+        if (isAtEnd())
+            return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd())
+            current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current-1);
+    }
+}
