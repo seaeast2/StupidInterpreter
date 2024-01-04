d443cc825c696ea6dab714bc5d8f3f414553a4f5
notmuchhayden <seaeast2@gmail.com>
2024-01-04 14:17:23 +0900

    Interpreter 작성중

--- sinterp/src/main/java/org/sealang/sinterp/Interpreter.java
+++ sinterp/src/main/java/org/sealang/sinterp/Interpreter.java
@@ -4,21 +4,121 @@ class Interpreter implements Expr.Visitor<Object> {
 
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left > (double)right;
+
+            case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left >= (double) right;
+
+            case LESS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left < (double) right;
+
+            case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left <= (double) right;
+
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+
+            case MINUS:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left - (double) right;
+
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double) right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String) right;
+                }
+
+                throw new RuntimeError(expr.operator,
+                        "Operands must be two number or two strings.");
+
+            case SLASH:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left / (double) right;
+
+            case STAR:
+                checkNumberOperands(expr.operator, left, right);
+                return (double)left * (double) right;
+        }
+
+        // unreachable
         return null;
     }
 
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
-        return null;
+        return evaluate(expr.expression);
     }
 
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
-        return null;
+        return expr.value;
     }
 
     @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                checkNumberOperand(expr.operator, right);
+                return -(double)right;
+        }
+
+        // unreachable
         return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double)
+            return;
+        throw new RuntimeError(operator, "Operand must be a number");
+    }
+
+    private void checkNumberOperands(Token operator,
+                                     Object left, Object right) {
+        if (left instanceof Double && right instanceof Double)
+            return;
+
+        throw new RuntimeError(operator, "Operands must be numbers");
+    }
+
+    // 명시적인 null, bool 빼고는 모두 ! 연산에서 true 로 간주된다.
+    private boolean isTruthy(Object object) {
+        if (object == null)
+            return false;
+        if (object instanceof Boolean)
+            return (boolean) object;
+        return true;
+    }
+
+    // 동등성 체크
+    private boolean isEqual(Object a, Object b) {
+        if (a == null && b == null)
+            return true;
+        if (a == null)
+            return false;
+
+        return a.equals(b);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
     }
 }
