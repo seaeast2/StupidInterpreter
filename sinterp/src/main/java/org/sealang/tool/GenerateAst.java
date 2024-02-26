5b3e6c0010b911d309c56bbf95e024160e5197ae
hlee.grapicar <seaeast2@gmail.com>
2024-02-26 17:19:46 +0900

    Assign Node 추가

--- sinterp/src/main/java/org/sealang/tool/GenerateAst.java
+++ sinterp/src/main/java/org/sealang/tool/GenerateAst.java
@@ -16,6 +16,7 @@ public class GenerateAst {
 
         // 문 Node 정의
         defineAst(outputDir, "Expr", Arrays.asList(
+                "Assign     : Token name, Expr value",
                 "Binary     : Expr left, Token operator, Expr right",
                 "Grouping   : Expr expression",
                 "Literal    : Object value",
