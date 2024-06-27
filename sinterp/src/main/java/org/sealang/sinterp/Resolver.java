671b9eb6b0495cf959b55634e0624b015cf3d12b
hlee.grapicar <seaeast2@gmail.com>
2024-06-27 17:13:17 +0900

    11.3.5 까지 완료 11.3.6 부터 시작하면 됨

--- sinterp/src/main/java/org/sealang/sinterp/Resolver.java
+++ sinterp/src/main/java/org/sealang/sinterp/Resolver.java
@@ -29,12 +29,45 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Void visitFunctionStmt(Stmt.Function stmt) {
+        declare(stmt.name); // 함수 이름을 현재 scope 에 등록
+        define(stmt.name); // 함수 이름을 리졸브
+
+        resolveFunction(stmt);
+        return null;
+    }
+
+    @Override
     public Void visitVarStmt(Stmt.Var stmt) {
         declare(stmt.name);
         if (stmt.initializer != null) {
             resolve(stmt.initializer);
         }
         define(stmt.name);
+        return null;
+    }
+
+    @Override
+    public Void visitAssignExpr(Expr.Assign expr) {
+        resolve(expr.value);
+        resolveLocal(expr, expr.name);
+        return null;
+    }
+
+    @Override
+    public Void visitVariableExpr(Expr.Variable expr) {
+        /*
+        * 변수가 현재 스코프에 존재하지만 값이 false면
+        * 변수가 선언은 되었지만 정의는 되지 않았다는 뜻이므로
+        * 에러로 처리한다.
+        * */
+        if (!scopes.isEmpty() &&
+        scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
+            SInterp.error(expr.name,
+                    "Can't read local variable in its own instruction.");
+        }
+
+        resolveLocal(expr, expr.name);
         return null;
     }
 
@@ -46,6 +79,16 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         expr.accept(this);
     }
 
+    private void resolveFunction(Stmt.Function function) {
+        beginScope();
+        for (Token param : function.params) {
+            declare(param);
+            define(param);
+        }
+        resolve(function.body);
+        endScope();
+    }
+
     private void beginScope() {
         scopes.push(new HashMap<String, Boolean>());
     }
@@ -54,6 +97,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         scopes.pop();
     }
 
+    // 이름 있는 객체(함수나 변수들)를 scope 에 등록
     private void declare(Token name) {
         if (scopes.isEmpty())
             return;
@@ -62,17 +106,31 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         scope.put(name.lexeme, false); // 선언만 되고 define 되지 않았기 때문에 false
     }
 
+    // 제공된 이름이 리졸브 되었음을 체크
     private void define(Token name) {
         if (scopes.isEmpty())
             return;
         scopes.peek().put(name.lexeme, true); // define 된 순간 true 로 변경
     }
 
-
-    @Override
-    public Void visitAssignExpr(Expr.Assign expr) {
-        return null;
+    private void resolveLocal(Expr expr, Token name) {
+        for (int i = scopes.size() - 1; i >= 0; i--) {
+            if (scopes.get(i).containsKey(name.lexeme)) {
+                interpreter.resolve(expr, scopes.size() - 1 - i);
+                return;
+            }
+        }
     }
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public Void visitBinaryExpr(Expr.Binary expr) {
@@ -104,10 +162,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         return null;
     }
 
-    @Override
-    public Void visitVariableExpr(Expr.Variable expr) {
-        return null;
-    }
+
 
 
 
@@ -116,10 +171,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
         return null;
     }
 
-    @Override
-    public Void visitFunctionStmt(Stmt.Function stmt) {
-        return null;
-    }
+
 
     @Override
     public Void visitIfStmt(Stmt.If stmt) {
